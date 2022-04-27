be324dc6ec93f49b1d28e18c281c647a7aae549d
kavat.lux@gmail.com <kavat.lux@gmail.com>
2022-04-26 21:30:43 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mongol-music-player
+A Mongol-style Music Player. Based on https://github.com/SamirPaul1/music.
---
+++ js/music-list.js
@@ -0,0 +1,45 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "ᠠᠬᠢ ᠨᠢ ᠦᠭᠡᠢ ᠭᠠᠵᠠᠷ",
+    artist: "ᠲᠩᠷᠢ",
+    img: "music-1",
+    src: "不长矮草的地方"
+  },
+  {
+    name: "ᠲᠠᠯ᠎ᠠ ᠨᠤᠲᠤᠭ",
+    artist: "ᠲᠩᠷᠢ",
+    img: "music-2",
+    src: "tal-nutag"
+  },
+  {
+    name: "ᠲᠠᠯ᠎ᠠ ᠨᠤᠲᠤᠭ",
+    artist: "ᠲᠩᠷᠢ",
+    img: "music-2",
+    src: "tal-nutag"
+  },
+  {
+    name: "ᠲᠠᠯ᠎ᠠ ᠨᠤᠲᠤᠭ",
+    artist: "ᠲᠩᠷᠢ",
+    img: "music-2",
+    src: "tal-nutag"
+  },
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
